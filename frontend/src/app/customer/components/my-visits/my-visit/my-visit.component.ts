c0a85c79b974ada383b511cd8ea068daa06932ad
youkouleley <hellblender@free.fr>
2018-04-28 22:30:53 +0200

    visit cancellation popup

--- frontend/src/app/customer/components/my-visits/my-visit/my-visit.component.ts
+++ frontend/src/app/customer/components/my-visits/my-visit/my-visit.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, TemplateRef } from '@angular/core';
 import { VisitStatusEnum } from '../../../../shared/common/enums/VisitStatusEnum';
 import { VisiteClass } from '../../../../core/models/VisiteClass';
 import { Router } from '@angular/router';
 import { VisiteService } from '../../../../shared/services/visite.service';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { NotificationsService } from 'angular2-notifications';
 
 @Component({
   selector: 'app-my-visit',
@@ -14,10 +16,14 @@ export class MyVisitComponent implements OnInit {
   @Input() visit: VisiteClass;
   @Input() datePassed: boolean;
   @Output() canceled: EventEmitter<any> = new EventEmitter<any>();
+  @ViewChild('cancelModal') cancelModalTemplate: TemplateRef<any>;
+  cancelModal: MatDialogRef<any>;
   VisitStatusEnum = VisitStatusEnum;
 
   constructor(
     private visiteService: VisiteService,
+    private dialog: MatDialog,
+    private notificationsService: NotificationsService,
     private router: Router
   ) { }
 
@@ -32,11 +38,23 @@ export class MyVisitComponent implements OnInit {
   }
 
   cancelClick() {
-    this.visiteService.cancel(this.visit.id).subscribe(res => {
-      this.canceled.emit();
-    }, err => {
+    this.cancelModal = this.dialog.open(this.cancelModalTemplate);
 
+    this.cancelModal.afterClosed().subscribe(value => {
+      if (value === 'OK') {
+        this.visiteService.cancel(this.visit.id).subscribe(res => {
+          this.notificationsService.success('Visite supprimée', 'Votre visite a bien été supprimée.');
+          this.canceled.emit();
+        }, err => {
+
+        });
+      }
     });
+
+
   }
 
+  closeCancelModal() {
+    this.cancelModal.close('cancel');
+  }
 }
