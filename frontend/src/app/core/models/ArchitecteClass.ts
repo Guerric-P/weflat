d494c7e345ef10ed2e96c494c11086f7f8bcb709
youkouleley <hellblender@free.fr>
2018-04-29 18:55:39 +0200

    iban validator

--- frontend/src/app/core/models/ArchitecteClass.ts
+++ frontend/src/app/core/models/ArchitecteClass.ts
@@ -5,20 +5,21 @@ import { VisiteClass } from "./VisiteClass";
 import { ArchitectSituationClass } from "./ArchitectSituationClass";
 
 export class ArchitecteClass extends UserClass{
-    public webSite : String;
-    public architectsOrder : boolean;
-    public cfai : boolean;
-    public professionalResponsibility : boolean;
-    public decennialInsurance : boolean;
-    public motivation : String;
-    public practicingSince : Date;
-    public type : ArchitectTypeClass;
-    public zipCodes : ZipCodeClass[];
-    public visites : VisiteClass[];
-    public potentialVisites : VisiteClass[];
+    public webSite: String;
+    public architectsOrder: boolean;
+    public cfai: boolean;
+    public professionalResponsibility: boolean;
+    public decennialInsurance: boolean;
+    public motivation: String;
+    public practicingSince: Date;
+    public type: ArchitectTypeClass;
+    public zipCodes: ZipCodeClass[];
+    public visites: VisiteClass[];
+    public potentialVisites: VisiteClass[];
     public situation: ArchitectSituationClass;
-    public cgu : boolean;
-    public status : number;
+    public cgu: boolean;
+    public status: number;
+    public iban: string
 
     constructor(obj?: any) {
         super();
