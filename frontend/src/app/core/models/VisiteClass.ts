12662c9cb2fdfe612cf55c042dad016b63cba2f0
youkouleley <hellblender@free.fr>
2018-05-08 20:17:03 +0200

    visit details in admin page

--- frontend/src/app/core/models/VisiteClass.ts
+++ frontend/src/app/core/models/VisiteClass.ts
@@ -1,6 +1,7 @@
 import { AcheteurClass } from "./AcheteurClass";
 import { ArchitecteClass } from "./ArchitecteClass";
 import { ZipCodeClass } from "./ZipCodeClass";
+import { VisitStatusEnum } from "../../shared/common/enums/VisitStatusEnum";
 
 export class VisiteClass {
     public id: number;
@@ -14,11 +15,11 @@ export class VisiteClass {
     public visiteDate: Date;
     public status: number;
     public announcementUrl: string;
+    public chargeId: string;
 
     get formattedAddress() {
         try {
-            return this.streetNumber
-                + ', '
+            return (this.streetNumber ? this.streetNumber + ', ' : '')
                 + this.route
                 + ' - '
                 + this.zipCode.number
@@ -27,6 +28,27 @@ export class VisiteClass {
         }
         catch (err) {
             // Ignore
+        }
+    }
+
+    get statusText() {
+        switch (this.status) {
+            case VisitStatusEnum.BEING_ASSIGNED:
+            return 'En cours d\'attribution';
+            case VisitStatusEnum.CANCELED:
+            return 'Annulée';
+            case VisitStatusEnum.IN_PROGRESS:
+            return 'En cours';
+            case VisitStatusEnum.REFUNDED:
+            return 'Remboursée';
+            case VisitStatusEnum.REPORT_AVAILABLE:
+            return 'Rapport disponible';
+            case VisitStatusEnum.REPORT_BEING_WRITTEN:
+            return 'Rapport en cours de rédaction';
+            case VisitStatusEnum.UNASSIGNED:
+            return 'Non assignée';
+            case VisitStatusEnum.WAITING_FOR_PAYMENT:
+            return 'En attente de paiement';
         }
     }
 
