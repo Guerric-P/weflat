c2b19caeb2e6a65810801ce8bbe168b32fff3165
youkouleley <hellblender@free.fr>
2018-05-31 23:11:39 +0200

    password forgotten forms validations

--- frontend/src/app/core/components/forgotten-password/forgotten-password.component.ts
+++ frontend/src/app/core/components/forgotten-password/forgotten-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../../../shared/services/user.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgotten-password',
@@ -16,10 +17,13 @@ export class ForgottenPasswordComponent implements OnInit {
   resetEmailSuccessful: boolean;
   resetEmailFailed: boolean;
   resetEmailButtonDisabled: boolean;
+  resetPasswordForm: FormGroup;
+  resetEmailForm: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
-    private userService: UserService
+    private userService: UserService,
+    private _formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
@@ -28,12 +32,20 @@ export class ForgottenPasswordComponent implements OnInit {
       .subscribe(params => {
         this.hash = params['hash'];
       });
+
+    this.resetPasswordForm = this._formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+    this.resetEmailForm = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
   }
 
-  changePassword(password: string) {
-    if (!this.resetPasswordButtonDisabled) {
+  changePassword() {
+    if (!this.resetPasswordButtonDisabled && !this.resetPasswordForm.invalid) {
       this.resetPasswordButtonDisabled = true;
-      this.userService.resetPassword(this.hash, password).subscribe(() => {
+      this.userService.resetPassword(this.hash, this.resetPasswordForm.value.password).subscribe(() => {
         this.resetPasswordSucccessful = true;
         this.resetPasswordFailed = false;
         this.resetPasswordButtonDisabled = false;
@@ -45,10 +57,10 @@ export class ForgottenPasswordComponent implements OnInit {
     }
   }
 
-  sendEmail(email: string) {
-    if (!this.resetEmailButtonDisabled) {
+  sendEmail() {
+    if (!this.resetEmailButtonDisabled && !this.resetEmailForm.invalid) {
       this.resetEmailButtonDisabled = true;
-      this.userService.forgottenPassword(email).subscribe(() => {
+      this.userService.forgottenPassword(this.resetEmailForm.value.email).subscribe(() => {
         this.resetEmailSuccessful = true;
         this.resetEmailFailed = false;
         this.resetEmailButtonDisabled = false;
@@ -57,6 +69,7 @@ export class ForgottenPasswordComponent implements OnInit {
         this.resetEmailFailed = true;
         this.resetEmailButtonDisabled = false;
       });
+
     }
   }
 }
