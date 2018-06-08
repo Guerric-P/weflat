5a4076a8cba92ce0c58965d26ea5fba69ddbde93
youkouleley <hellblender@free.fr>
2018-06-08 23:48:25 +0200

    linting

--- frontend/src/app/core/models/RenovationClass.ts
+++ frontend/src/app/core/models/RenovationClass.ts
@@ -1,10 +1,10 @@
 import { PositionClass } from './PositionClass';
 
 export class RenovationClass {
-	public id: number;
-	public position: PositionClass;
-	public condition: number;
-	public remarks: string;
+    public id: number;
+    public position: PositionClass;
+    public condition: number;
+    public remarks: string;
     public estimatedWork: number;
 
     constructor(obj?: any) {
