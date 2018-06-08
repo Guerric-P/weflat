79a7e0fdadc8b5c6ee6a0346bbb1335dbc21f77f
youkouleley <hellblender@free.fr>
2018-06-08 23:17:17 +0200

    linting

--- frontend/src/app/shared/resolvers/payment-type.resolver.ts
+++ frontend/src/app/shared/resolvers/payment-type.resolver.ts
@@ -9,7 +9,8 @@ export class PaymentTypeResolver implements Resolve<PaymentTypeClass[]> {
 
     constructor(private paymentTypeService: PaymentTypeService) { }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): PaymentTypeClass[] | Observable<PaymentTypeClass[]> | Promise<PaymentTypeClass[]> {
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+        PaymentTypeClass[] | Observable<PaymentTypeClass[]> | Promise<PaymentTypeClass[]> {
         return this.paymentTypeService.getAll();
     }
 }
