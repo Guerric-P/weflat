a47896772245137b203fcfb8e6d7565b40712a52
youkouleley <hellblender@free.fr>
2018-06-19 01:01:40 +0200

    responsive edit visit popup

--- frontend/src/app/customer/components/edit-visit-popup/edit-visit-popup.component.ts
+++ frontend/src/app/customer/components/edit-visit-popup/edit-visit-popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Inject, EventEmitter, Output, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { VisitClass } from '../../../core/models/VisitClass';
@@ -6,19 +6,22 @@ import { GooglePlaceKeys } from '../../../shared/common/GooglePlaceKeys';
 import { values } from '../../../shared/common/TimeDropDownValues';
 import { ZipCodeClass } from '../../../core/models/ZipCodeClass';
 import { VisitService } from '../../../shared/services/visit.service';
-declare var google: any;
+declare var google;
+declare var moment;
 
 @Component({
   selector: 'app-edit-visit-popup',
   templateUrl: './edit-visit-popup.component.html',
   styleUrls: ['./edit-visit-popup.component.scss']
 })
-export class EditVisitPopupComponent implements OnInit {
+export class EditVisitPopupComponent implements OnInit, OnDestroy {
 
   visitForm: FormGroup;
   visit: VisitClass;
   @ViewChild('addressInput') addressInput: ElementRef;
   @Output() onUpdate = new EventEmitter<VisitClass>();
+  mutationObserver: MutationObserver;
+  minDate = moment().add(1, 'days').toDate();
 
   times = values;
 
@@ -42,6 +45,7 @@ export class EditVisitPopupComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
     this.visit = this.data.visit;
 
     const autocomplete = new google.maps.places.Autocomplete(this.addressInput.nativeElement, this.options);
@@ -51,26 +55,64 @@ export class EditVisitPopupComponent implements OnInit {
 
     this.visitForm = this.fb.group({
       addressInput: [null],
-      streetNumber: [{value: this.visit.streetNumber, disabled: true}, Validators.required],
-      route: [{value: this.visit.route, disabled: true}, Validators.required],
-      zipCode: [{value: this.visit.zipCode.number, disabled: true}, Validators.required],
-      city: [{value: this.visit.city, disabled: true}, Validators.required],
+      streetNumber: [{ value: this.visit.streetNumber, disabled: true }, Validators.required],
+      route: [{ value: this.visit.route, disabled: true }, Validators.required],
+      zipCode: [{ value: this.visit.zipCode.number, disabled: true }, Validators.required],
+      city: [{ value: this.visit.city, disabled: true }, Validators.required],
       time: [this.currentTimeIndex, Validators.required],
       date: [this.visit.visiteDate, Validators.required],
-      announcementUrl: [this.visit.announcementUrl]
+      announcementUrl: [this.visit.announcementUrl, Validators.pattern(/^(http|https):\/\/[^ "]+$/)]
     });
+
+    this.mutationObserver = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+
+        let target = <HTMLElement>mutation.target;
+        let oldTop = mutation.oldValue && mutation.oldValue.split(';').map(x => x.split(':').map(y => y.trim())).find(z => z[0] === 'top');
+        let oldTopValue = oldTop && oldTop[1];
+
+        if (target.classList.contains('pac-container')
+          && target.style.top
+          && !target.classList.contains('weflat-touched')
+          && target.style.top != oldTopValue) {
+          this.fixAutocompletePosition(target);
+        }
+        else {
+          target.classList.remove('weflat-touched');
+        }
+      });
+    });
+    this.mutationObserver.observe(document, { subtree: true, attributes: true, attributeFilter: ["style"], attributeOldValue: true });
   }
 
+  ngOnDestroy(): void {
+    this.mutationObserver.disconnect();
+  }
+
+
+  fixAutocompletePosition(element: HTMLElement) {
+    let extractOffsetRegex = /^([-\d]*)px$/;
+
+    let pacContainerOffsetString = element.style.top;
+    let pacContainerOffset = +extractOffsetRegex.exec(pacContainerOffsetString)[1];
+    let htmlOffsetString = document.documentElement.style.top;
+    let htmlOffset = +extractOffsetRegex.exec(htmlOffsetString)[1];
+    pacContainerOffset -= htmlOffset;
+    element.style.top = pacContainerOffset + 'px';
+    element.classList.add('weflat-touched');
+  }
 
   onSubmit() {
-    this.loadVisit();
+    if (this.visitForm.valid) {
+      this.loadVisit();
 
-    this.visitService.patch(this.visit, this.visit.id).subscribe(res => {
-      this.onUpdate.emit(res);
-    });
+      this.visitService.patch(this.visit, this.visit.id).subscribe(res => {
+        this.onUpdate.emit(res);
+      });
+    }
   }
 
-  
+
   loadVisit() {
     this.visit.city = this.visitForm.controls['city'].value;
     this.visit.route = this.visitForm.controls['route'].value;
