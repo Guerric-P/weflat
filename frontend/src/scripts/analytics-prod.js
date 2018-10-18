2e7c9d248e394e0dbc913a54d6ea45b04114c16e
youkouleley <hellblender@free.fr>
2018-10-18 19:23:14 +0200

    google analytics

---
+++ frontend/src/scripts/analytics-prod.js
@@ -0,0 +1,13 @@
+var googleId = 'UA-79254611-1';
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.async = true;
+script.src = `https://www.googletagmanager.com/gtag/js?id=${googleId}`;
+head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', googleId);
