92488341f4a2ffddbb9e150c4c20ea5d861f5dbd
youkouleley <hellblender@free.fr>
2018-11-13 19:49:30 +0100

    404 page

--- frontend/src/app/server-errors/not-found/not-found.component.ts
+++ frontend/src/app/server-errors/not-found/not-found.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit, Optional, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { RESPONSE } from '@nguniversal/express-engine/tokens';
-import { Response } from 'express';
+import { RESPONSE, REQUEST } from '@nguniversal/express-engine/tokens';
+import { Response, Request } from 'express';
 
 @Component({
   selector: 'app-not-found',
@@ -10,8 +10,11 @@ import { Response } from 'express';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object,
-    @Optional() @Inject(RESPONSE) private response: Response) {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Optional() @Inject(RESPONSE) private response: Response,
+    @Optional() @Inject(REQUEST) private request: Request,
+  ) {
   }
 
 
