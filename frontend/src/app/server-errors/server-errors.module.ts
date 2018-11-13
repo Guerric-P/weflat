6a9fe6dd3f1774b26ca7ac123f462d73c8bf12a2
youkouleley <hellblender@free.fr>
2018-11-13 19:49:30 +0100

    404 page

--- frontend/src/app/server-errors/server-errors.module.ts
+++ frontend/src/app/server-errors/server-errors.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorsRoutingModule } from './server-errors-routing.module';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [NotFoundComponent],
   imports: [
     CommonModule,
-    ServerErrorsRoutingModule
+    ServerErrorsRoutingModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class ServerErrorsModule { }
