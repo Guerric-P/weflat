ce651eee84f20cc73266c6bd9f1bcce07394c6e5
youkouleley <hellblender@free.fr>
2018-11-20 19:37:27 +0100

    environment variables

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 
 set -e
 cd /app
-pm2 start dist/server.js --no-daemon
+pm2 start dist/server.js --no-daemon -o="/app/log/output.log" -e="/app/log/error.log"
 checkerror "Echec de pm2 start"
 sh -c "$@"
