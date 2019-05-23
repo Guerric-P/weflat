30430a22112b9d486abbbc25b486675691284a81
youkouleley <hellblender@free.fr>
2019-05-23 23:59:44 +0200

    update cli

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:8080',
+  backendUrl: 'http://localhost',
   baseBackendUrl: '/backend',
   stripePublicKey: 'pk_test_EJlsBsLshUf7TNnB2ITpQ7sB',
   googleMapsUrl: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAK7jAiHuz-NhGcZWd8mwdRBQWzNTkfXyY&libraries=places',
