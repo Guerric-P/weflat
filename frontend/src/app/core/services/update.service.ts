08cdc5747153a2cc3aec251f6843f7124675cbfc
youkouleley <hellblender@free.fr>
2019-06-02 17:57:32 +0200

    Update update.service.ts

--- frontend/src/app/core/services/update.service.ts
+++ frontend/src/app/core/services/update.service.ts
@@ -1,31 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { MatSnackBar } from '@angular/material';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateService {
 
-  constructor(private swUpdate: SwUpdate, private snackbar: MatSnackBar) {
-    this.swUpdate.available.subscribe(evt => {
-      console.log('Update available');
-      const snack = this.snackbar.open('Update Available', 'Reload');
+  constructor(
+    private swUpdate: SwUpdate,
+    private snackbar: MatSnackBar,
+    @Inject(PLATFORM_ID) platformId: string
+  ) {
+    if (isPlatformBrowser(platformId)) {
+      this.swUpdate.available.subscribe(evt => {
+        console.log('Update available');
+        const snack = this.snackbar.open('Update Available', 'Reload');
 
-      snack
-        .onAction()
-        .subscribe(() => {
-          window.location.reload();
-        });
+        snack
+          .onAction()
+          .subscribe(() => {
+            window.location.reload();
+          });
 
-      setTimeout(() => {
-        snack.dismiss();
-      }, 6000);
-    });
+        setTimeout(() => {
+          snack.dismiss();
+        }, 6000);
+      });
 
-    setInterval(() => {
-      console.log('Starting update check');
-      this.swUpdate.checkForUpdate().then(() => console.log('Update check done.'));
-    }, 5000);
+      setInterval(() => {
+        console.log('Starting update check');
+        this.swUpdate.checkForUpdate().then(() => console.log('Update check done.'));
+      }, 5000);
+    }
   }
 }
