18b6c03467ce3cbfeb05d378db854e14890e67b2
youkouleley <hellblender@free.fr>
2019-09-06 23:44:58 +0200

    Update zip-codes.component.ts

--- frontend/src/app/admin/components/zip-codes/zip-codes/zip-codes.component.ts
+++ frontend/src/app/admin/components/zip-codes/zip-codes/zip-codes.component.ts
@@ -154,4 +154,8 @@ export class ZipCodesComponent implements OnInit {
   trackByFn(index: number, item: ZipCodeClass) {
     return item.id;
   }
+
+  closeCancelModal() {
+    this.confirmModal.close();
+  }
 }
