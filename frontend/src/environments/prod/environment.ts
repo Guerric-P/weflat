077d8885137cd0e7aa903df38a922c161bd49f53
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2020-10-27 22:53:54 +0100

    kuberneted pod names

--- frontend/src/environments/prod/environment.ts
+++ frontend/src/environments/prod/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://server.default.svc.cluster.local:8080',
+  backendUrl: 'http://server.default.pod.cluster.local:8080',
   baseBackendUrl: '/backend',
   stripePublicKey: 'pk_live_JeIWSJo8RkArMLJ1wjU0qS9j',
   googleMapsUrl: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAK7jAiHuz-NhGcZWd8mwdRBQWzNTkfXyY&libraries=places',
