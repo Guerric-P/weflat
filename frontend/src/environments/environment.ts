0223686d1c5f9b1f04b1201aae57f4e38089f15f
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2020-10-27 23:43:19 +0100

    services DNS names

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  backendUrl: 'http://server.default.pod.cluster.local:8080',
+  backendUrl: 'http://server-svc:8080',
   baseBackendUrl: '/backend',
   stripePublicKey: 'pk_test_EJlsBsLshUf7TNnB2ITpQ7sB',
   googleMapsUrl: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAK7jAiHuz-NhGcZWd8mwdRBQWzNTkfXyY&libraries=places',
