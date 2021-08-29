9cff0c9e1b65bfdd66bc80725e883147023f3d33
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2021-08-29 15:54:06 +0200

    migrate to bootstrap 5

--- frontend/src/typings.d.ts
+++ frontend/src/typings.d.ts
@@ -1,10 +1,5 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
-declare module 'jwt-decode' {
-	function decode(token: string): any;
-	namespace decode { }
-	export = decode;
-}
 interface NodeModule {
 	id: string;
 }
