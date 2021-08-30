8efd1ee4806da7cd4093ed35fb2342e4b5fb1e0e
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2021-08-30 09:58:50 +0200

    remove baseBackendUrl

--- frontend/src/environments/prod/environment.ts
+++ frontend/src/environments/prod/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://server-svc:8080',
-  baseBackendUrl: '/backend',
+  backendUrl: 'http://server-svc',
   stripePublicKey: 'pk_live_JeIWSJo8RkArMLJ1wjU0qS9j',
   googleMapsUrl: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAK7jAiHuz-NhGcZWd8mwdRBQWzNTkfXyY&libraries=places',
   displayTestWarning: false
--- frontend/src/environments/test/environment.ts
+++ frontend/src/environments/test/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: true,
-    backendUrl: 'http://server-svc:8080',
-    baseBackendUrl: '/backend',
+    backendUrl: 'http://server-svc',
     stripePublicKey: 'pk_test_EJlsBsLshUf7TNnB2ITpQ7sB',
     googleMapsUrl: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAK7jAiHuz-NhGcZWd8mwdRBQWzNTkfXyY&libraries=places',
     displayTestWarning: true
