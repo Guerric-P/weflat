8e208cd33b2a1c8103188e490b2384cef397b67f
Guerric Phalippou <guerric.phalippou@soprasteria.com>
2021-08-31 17:33:29 +0200

    Revert "remove baseBackendUrl"
    This reverts commit 8efd1ee4806da7cd4093ed35fb2342e4b5fb1e0e.

--- frontend/src/environments/prod/environment.ts
+++ frontend/src/environments/prod/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  backendUrl: 'http://server-svc',
+  backendUrl: 'http://server-svc:8080',
+  baseBackendUrl: '/backend',
   stripePublicKey: 'pk_live_JeIWSJo8RkArMLJ1wjU0qS9j',
   googleMapsUrl: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAK7jAiHuz-NhGcZWd8mwdRBQWzNTkfXyY&libraries=places',
   displayTestWarning: false
